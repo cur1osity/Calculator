0d31d5eff3a8ff9e55f96be943fde6b45190ab1a
cur1osity <marcin.gajc@gmail.com>
2017-09-28 16:29:04 +0200

    Calculator ver 1.0

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,42 @@
+package com.kodilla.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    private double a;
+    private double b;
+    private double answer;
+
+    public void addition() {
+
+        this.operation();
+        answer = a + b;
+
+        System.out.println("--------------------");
+        System.out.println(a + " + " + b + " = " + answer );
+        System.out.println("--------------------");
+    }
+
+    public void subtraction() {
+
+        this.operation();
+        answer = a - b;
+
+        System.out.println("--------------------");
+        System.out.println(a + " - " + b + " = " + answer );
+        System.out.println("--------------------");
+    }
+
+    private void operation() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        a = scanner.nextDouble();
+
+        System.out.println("Enter second number: ");
+        b = scanner.nextDouble();
+    }
+}
+
---
+++ src/main/java/com/kodilla/calculator/Main.java
@@ -0,0 +1,51 @@
+package com.kodilla.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice;
+        boolean notQuit = true;
+
+        while (notQuit) {
+
+            printList();
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 0:
+                    notQuit = false;
+                    break;
+
+                case 1:
+                    System.out.println("Addition selected.");
+                    calculator.addition();
+                    break;
+
+                case 2:
+                    System.out.println("Subtraction selected.");
+                    calculator.subtraction();
+                    break;
+                default:
+                    System.out.println("Enter correct number!");
+
+            }
+        }
+    }
+
+    private static void printList() {
+        System.out.println("\nPress: ");
+        System.out.println("\t 1 for (+)");
+        System.out.println("\t 2 for (-)");
+        System.out.println("\t 0 - To quit the application.");
+
+    }
+}
+
+
